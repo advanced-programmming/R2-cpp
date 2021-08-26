ec52a99aad3fff602f8056ea477dce9e7e615b4e
“TheLuisMeli” <ext_luboliva@mercadolibre.com.co>
2021-08-26 17:41:40 -0500

    finished

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # R2-cpp
 cpp 
+
+
+Para compilar en c++: g++ -Wall -o r2 -std=c++11 r2.cpp
+
+Para ejecutar:
+
+./r2
---
+++ r2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    int r1 = 0, s =0;
+    cin>>r1>>s;
+    int r2 = (s * 2) - r1;
+    cout<<r2<<endl;
+    return 0;
+}
